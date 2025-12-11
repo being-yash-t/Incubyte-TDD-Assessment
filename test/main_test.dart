011896dae01c814d40df0f803bc5192470c65be3
Yash Thakur <yasht3210@gmail.com>
2025-12-11 12:16:17 +0530

    Added failing test for add with blank string.

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  group('Add Functionality', () {
+    test('should return 0 when add is called with empty string', () {
+      // arrage
+      final inputString = '';
+
+      // act
+      final result = add(inputString);
+
+      // verify
+      expect(result, 0);
+    });
+  });
+}
