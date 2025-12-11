0932ba9891a4ce665dc061ccd11a20acef47b7eb
Yash Thakur <yasht3210@gmail.com>
2025-12-11 12:19:28 +0530

    Added implementation for empty string input.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
 void main(List<String> arguments) {}
+
+int add(String numbers) {
+  if (numbers.trim().isEmpty) {
+    return 0;
+  }
+  throw UnimplementedError();
+}
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,13 +1,14 @@
+import 'package:incubyte_tdd_assignment/main.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Add Functionality', () {
     test('should return 0 when add is called with empty string', () {
       // arrage
-      final inputString = '';
+      final emptyString = '';
 
       // act
-      final result = add(inputString);
+      final result = add(emptyString);
 
       // verify
       expect(result, 0);
