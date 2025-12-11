81cd2307e4501e3ef52c0410767d27545544a717
Yash Thakur <yasht3210@gmail.com>
2025-12-11 14:33:54 +0530

    Added main implementation with progressive comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,28 @@ import 'package:incubyte_tdd_assignment/exceptions.dart';
 
 import 'utils.dart' as utils;
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  print("--------------------------------");
+  print("Incubyte TDD Assessment");
+  print("--------------------------------");
+  if (arguments.isNotEmpty) {
+    try {
+      String numbers = arguments.first;
+
+      // when passing \n from command line, it is escaped to \\n, so we need to replace it with \n
+      numbers = numbers.replaceAll('\\n', '\n');
+
+      final result = add(numbers);
+      print("Result: $result");
+    } on NegativeNumberException catch (e) {
+      print("Negative numbers not allowed: ${e.negativeNumbers}");
+    } catch (e) {
+      print("An unknown error occurred: $e");
+    }
+  } else {
+    print("No arguments provided");
+  }
+}
 
 int add(String numbers) {
   if (numbers.trim().isEmpty) {
@@ -23,6 +44,8 @@ int add(String numbers) {
   // convert <String>[] into <int>[]
   final parsedNumbers = numbersList.map(int.parse);
 
+  print("Parsed numbers: $parsedNumbers");
+
   checkForNegatives(parsedNumbers);
 
   final sum = parsedNumbers.reduce(utils.add);
@@ -32,9 +55,7 @@ int add(String numbers) {
 void checkForNegatives(Iterable<int> numbers) {
   final negatives = numbers.where((n) => n.isNegative);
   if (negatives.isNotEmpty) {
-    final exception = NegativeNumberException(negatives);
-    print(exception);
-    throw exception;
+    throw NegativeNumberException(negatives);
   }
 }
 
@@ -42,6 +63,7 @@ void checkForNegatives(Iterable<int> numbers) {
   if (numbers.startsWith('//')) {
     final newLineIndex = numbers.indexOf('\n');
     final delimiter = numbers.substring(2, newLineIndex);
+    print("Found delimiter '$delimiter'");
     return (delimiter, numbers.substring(newLineIndex + 1));
   }
   return (null, numbers);
